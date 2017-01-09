7505d48e4351c77e4c54486e103fa8cf4de87367
Andy <andy9775@gmail.com>
2017-01-09 15:55:24 -0500

    update to hola, mundo!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def hello
-	  render html: 'Hello world¡'
+	  render html: 'hola, mundo!'
 	end
 
 	def goodbye
